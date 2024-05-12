95f72af15f313b63879e2bf3dcae8472b81a0121
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-12 16:19:50 +0530

    Home: Animation Gallery Added

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,26 @@ import { type Metadata } from "next"
 
 import generateMetadata from "utils/seo"
 
+import AnimationGallery from "components/AnimationGallery"
+import Footer from "@/components/Footer"
+
 export const metadata: Metadata = generateMetadata({
   path: "/",
   title: "Next.js App",
-  description: "A gallery of TailwindCSS animations that you can just copy-paste into your projects",
+  description:
+    "A gallery of TailwindCSS animations that you can just copy-paste into your projects",
 })
 
 const Home: React.FC = () => {
   return (
-    <main className="grid h-screen place-content-center">
-      <h1 className="text-center text-xl font-medium text-zinc-300">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-    </main>
+    <>
+      <main className="grid h-screen place-content-center">
+        <h1 className="text-center text-xl font-medium text-zinc-300">
+          <AnimationGallery />
+        </h1>
+      </main>
+      <Footer />
+    </>
   )
 }
 
