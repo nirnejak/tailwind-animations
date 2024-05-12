a3e8add5f49ce5160884884e81cf7996efc64c34
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-13 02:53:32 +0530

    Animation Card: animation class applied to right element

--- components/AnimationCard.tsx
+++ components/AnimationCard.tsx
@@ -16,16 +16,15 @@ const AnimationCard: React.FC<Props> = ({ color, title, animationClass }) => {
   return (
     <div className="rounded-xl bg-zinc-800 text-center px-18 pb-20 pt-24 relative">
       <button
-        className={classNames(
-          "absolute top-2 right-2 rounded-md p-2 text-zinc-300 hover:bg-zinc-950 hover:text-zinc-200 focus:bg-zinc-950 focus:outline-none",
-          animationClass
-        )}
+        className="absolute top-2 right-2 rounded-md p-2 text-zinc-300 hover:bg-zinc-950 hover:text-zinc-200 focus:bg-zinc-950 focus:outline-none"
         onClick={() => copyToClipboard(animationClass)}
       >
         <Copy size={16} />
       </button>
       <p className="text-sm absolute top-4 left-5">{title}</p>
-      <Button variant={color}>Click Me</Button>
+      <Button variant={color} className={animationClass}>
+        Click Me
+      </Button>
     </div>
   )
 }
