5cb52931209468db8b6000c292cc28a160ab603c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-12 17:25:08 +0530

    Button: info option added

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import classNames from "utils/classNames"
 
 interface Props {
-  variant?: "primary" | "danger" | "success" | "warning" | "purple"
+  variant?: "primary" | "danger" | "success" | "warning" | "info" | "purple"
   children: React.ReactNode
   type?: "reset" | "button" | "submit"
   className?: string
@@ -31,6 +31,8 @@ const Button: React.FC<Props> = ({
         return "bg-green-600 text-green-50 hover:bg-green-700 focus:bg-green-700"
       case "warning":
         return "bg-amber-600 text-amber-50 hover:bg-amber-700 focus:bg-amber-700"
+      case "info":
+        return "bg-sky-600 text-sky-50 hover:bg-sky-700 focus:bg-sky-700"
       case "primary":
       default:
         return "bg-gray-100 text-gray-700 hover:bg-gray-300 focus:bg-gray-300"
