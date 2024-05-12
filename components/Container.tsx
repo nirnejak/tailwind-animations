0f60a3788a22fa0f3ee28d6de00c4dbda0cb0308
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-12 16:13:13 +0530

    Container component added

---
+++ components/Container.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+
+import classNames from "utils/classNames"
+
+interface Props {
+  children: React.ReactNode
+  className?: string
+  size?: "narrow" | "normal" | "wide" | "ultrawide"
+}
+
+const Container: React.FC<Props> = ({
+  children,
+  className,
+  size = "ultrawide",
+  ...restProps
+}) => {
+  const widthClass = React.useMemo(() => {
+    switch (size) {
+      case "narrow":
+        return "max-w-[480px]"
+      case "wide":
+        return "max-w-[980px]"
+      case "ultrawide":
+        return "max-w-[1200px]"
+      case "normal":
+      default:
+        return "max-w-[680px]"
+    }
+  }, [size])
+
+  return (
+    <div
+      className={classNames("mx-auto px-4 md:px-0", widthClass, className)}
+      {...restProps}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Container
