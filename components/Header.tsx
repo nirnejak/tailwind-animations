a017036d462bf734947db911ffbf8ec68d9710da
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-12 16:26:17 +0530

    Header component added

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+import { Gear } from "akar-icons"
+import Link from "next/link"
+
+import Container from "./Container"
+import Button from "./atoms/Button"
+
+const Header: React.FC = () => {
+  return (
+    <header className="fixed top-0 w-full pt-5">
+      <Container>
+        <div className="flex items-center justify-between">
+          <Link href={"/"} className="flex text-zinc-300">
+            <span className="font-light">Tailwind</span>
+            <span className="font-semibold">Animations</span>
+          </Link>
+          <div className="flex">
+            <Link href={"/home"}>
+              <Button>Try now</Button>
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
