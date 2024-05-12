3b68b89948d031a5e26fe60afe55f3123ef5f831
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-13 00:41:32 +0530

    Animation Gallery: Search Feature Added

--- components/AnimationGallery.tsx
+++ components/AnimationGallery.tsx
@@ -7,24 +7,41 @@ import ColorSelector from "./atoms/ColorSelector"
 import { IColorVariants } from "./atoms/Button"
 import AnimationCard from "./AnimationCard"
 import Container from "./Container"
+import Input from "./atoms/Input"
 
 interface Props {}
 
 const AnimationGallery: React.FC<Props> = () => {
   const [color, setColor] = React.useState<IColorVariants>("violet")
+  const [search, setSearch] = React.useState("")
 
   return (
     <Container>
       <div className="mt-32">
         <ColorSelector color={color} setColor={setColor} />
+        <div className="mt-5">
+          <Input
+            type="text"
+            id="search-input"
+            placeholder="Search Animation..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
         <div className="grid grid-cols-5 gap-3 mt-5">
-          {animations.map((animation) => (
-            <AnimationCard
-              color={color}
-              title={animation.title}
-              animationClass={animation.animationClass}
-            />
-          ))}
+          {animations
+            .filter((animation) =>
+              animation.title
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            )
+            .map((animation) => (
+              <AnimationCard
+                color={color}
+                title={animation.title}
+                animationClass={animation.animationClass}
+              />
+            ))}
         </div>
       </div>
     </Container>
