e9a28821c9fcb3773c9765b7c6624e6324de5929
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-12 16:42:31 +0530

    Animation gallery layout fixes

--- components/AnimationGallery.tsx
+++ components/AnimationGallery.tsx
@@ -4,22 +4,28 @@ import * as React from "react"
 import Button from "./atoms/Button"
 import Input from "./atoms/Input"
 import AnimationCard from "./AnimationCard"
+import Container from "./Container"
 
 interface Props {}
 
 const AnimationGallery: React.FC<Props> = () => {
   return (
-    <div className="grid grid-cols-5 gap-3">
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-      <AnimationCard />
-    </div>
+    <Container>
+      <div className="grid grid-cols-4 gap-3">
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+        <AnimationCard />
+      </div>
+    </Container>
   )
 }
 
