f4b2a965452cf1c2a4b4c6e85f7cb4546c765102
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-12 16:40:33 +0530

    Animation Gallery Updated

--- components/AnimationGallery.tsx
+++ components/AnimationGallery.tsx
@@ -3,26 +3,22 @@ import * as React from "react"
 
 import Button from "./atoms/Button"
 import Input from "./atoms/Input"
+import AnimationCard from "./AnimationCard"
 
 interface Props {}
 
 const AnimationGallery: React.FC<Props> = () => {
   return (
-    <div>
-      <div className="mb-3">
-        <Input id="input" type="text" placeholder="Enter something..." />
-      </div>
-      <div>
-        <Button
-          variant="primary"
-          type="button"
-          onClick={() => {
-            console.log("copy this")
-          }}
-        >
-          Hello
-        </Button>
-      </div>
+    <div className="grid grid-cols-5 gap-3">
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
+      <AnimationCard />
     </div>
   )
 }
