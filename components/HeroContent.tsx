0683273fb4e1d5b95603c6514e6a7045327693a6
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-03 21:50:40 +0530

    Hero style updated

--- components/HeroContent.tsx
+++ components/HeroContent.tsx
@@ -2,15 +2,13 @@ import * as React from "react"
 
 import Container from "components/Container"
 
-interface Props {}
-
-const HeroContent: React.FC<Props> = () => {
+const HeroContent: React.FC = () => {
   return (
-    <Container className="my-40">
-      <h1 className="text-4xl mb-5 text-zinc-100 font-medium">
+    <Container className="my-40 text-center">
+      <h1 className="mb-5 text-6xl font-medium leading-tight text-zinc-100">
         Copy Paste animations for TailwindCSS
       </h1>
-      <p>
+      <p className="mx-auto max-w-[560px] text-xl">
         Explore a curated collection of TailwindCSS animation classes. Just copy
         and paste them into your code for free.
       </p>
