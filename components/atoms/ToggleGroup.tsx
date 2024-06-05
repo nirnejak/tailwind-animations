eb6e245183939b85b04fabf72902f9de681ffff8
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-05 21:18:49 +0530

    refactor(components): ColorSelector to ToggleGroup

---
+++ components/atoms/ToggleGroup.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+
+import * as ToggleGroup from "@radix-ui/react-toggle-group"
+
+interface Props {
+  options: React.ReactNode[]
+  value: string
+  setValue: (val) => void
+}
+
+const CustomToggleGroup: React.FC<Props> = ({ options, value, setValue }) => {
+  return (
+    <div>
+      <ToggleGroup.Root
+        className="inline-flex space-x-px rounded"
+        type="single"
+        defaultValue={value}
+        onValueChange={setValue}
+        aria-label="Button Themes"
+      >
+        {options.map((option, index) => (
+          <ToggleGroup.Item
+            key={index}
+            className="flex items-center justify-center bg-zinc-800 p-3 text-base leading-4 first:rounded-l-lg last:rounded-r-lg hover:bg-zinc-950 focus:z-10 focus:outline-none data-[state=on]:bg-zinc-700"
+            value={value}
+            aria-label={value}
+          >
+            {value}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
+    </div>
+  )
+}
+
+export default CustomToggleGroup
