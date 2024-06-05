5596867687e036bf136c50b9dc6c86c860af7401
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-05 19:39:32 +0530

    Header: github link removed, in development message removed, creator style updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,6 @@ import * as React from "react"
 import { motion } from "framer-motion"
 import Link from "next/link"
 
-// import { GithubFill, LinkOut } from "akar-icons"
-
 import Container from "./Container"
 
 const Header: React.FC = () => {
@@ -15,41 +13,22 @@ const Header: React.FC = () => {
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 1, delay: 0.4, type: "spring" }}
     >
-      <div className="bg-violet-500 p-3 text-xs text-white">
-        <Container className="text-center">
-          This project is still in development. Please take a minute to submit
-          your feedback!
-        </Container>
-      </div>
       <Container className="mt-5">
         <div className="flex items-center justify-between">
           <Link href={"/"} className="flex text-zinc-100">
             <span className="font-light">Tailwind</span>
             <span className="font-semibold">Animations</span>
           </Link>
-          <div className="flex">
-            {/* <Link
-              href={"https://github.com/nirnejak/tailwind-animations/"}
+          <p className="text-sm">
+            <span>Designed and built by </span>
+            <Link
+              href={"https://nirnejak.com/"}
               target="_blank"
+              className="font-medium text-zinc-300 underline underline-offset-4 hover:no-underline"
             >
-              <button className="flex gap-2 items-center rounded-md p-2 text-sm px-3 py-2 bg-zinc-900 hover:bg-zinc-950 hover:text-zinc-200 focus:bg-zinc-950 focus:outline-none transition-colors -mt-2 -mr-3">
-                <span>View Source</span>
-                <span>
-                  <GithubFill size={16} />
-                </span>
-              </button>
-            </Link> */}
-            <p className="text-sm">
-              <span>Designed and built by </span>
-              <Link
-                href={"https://nirnejak.com/"}
-                target="_blank"
-                className="underline underline-offset-4 hover:no-underline"
-              >
-                Jitendra Nirnejak
-              </Link>
-            </p>
-          </div>
+              Jitendra Nirnejak
+            </Link>
+          </p>
         </div>
       </Container>
     </motion.header>
