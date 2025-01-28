b99dad43b8f0ebadd48ac0fca0ccfd212fcaff29
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-28 15:41:09 +0530

    FeedbackWidget: layout updates

--- components/FeedbackWidget.tsx
+++ components/FeedbackWidget.tsx
@@ -2,13 +2,13 @@
 import * as React from "react"
 
 import { GithubFill, ThumbsUp } from "akar-icons"
-import Link from "next/link"
 
 import useClickOutside from "@/hooks/useClickOutside"
 
 import Button from "@/components/atoms/Button"
 import Input from "@/components/atoms/Input"
 import Textarea from "@/components/atoms/Textarea"
+import Link from "next/link"
 
 interface ResponseDataType {
   ok: boolean
@@ -83,7 +83,7 @@ const FeedbackWidget: React.FC = () => {
     return (
       <div
         ref={ref}
-        className="fixed bottom-4 right-4 max-w-[400px] animate-scale rounded-lg bg-zinc-700 p-5 text-sm md:min-w-[400px]"
+        className="fixed bottom-4 right-4 w-[320px] animate-scale rounded-lg bg-zinc-700 p-5 text-sm md:min-w-[400px]"
         style={{
           animationIterationCount: "1",
           transformOrigin: "bottom right",
@@ -122,14 +122,14 @@ const FeedbackWidget: React.FC = () => {
             required
             className="mb-2"
           />
-          <div className="flex items-center gap-4 text-sm">
-            <Button type="submit" disabled={isSending}>
+          <div className="flex flex-col gap-2">
+            <Button type="submit" disabled={isSending} className="w-full">
               {isSending ? "Sending..." : isSent ? "Sent!" : "Send Feedback"}
             </Button>
-            <span className="text-zinc-400">or</span>
+            <div className="text-center text-xs">or</div>
             <Link
               href="https://github.com/nirnejak/tailwind-animations/issues/new"
-              className="flex items-center gap-1.5 rounded-lg px-4 py-2 text-zinc-400 transition-all hover:bg-zinc-800"
+              className="flex items-center outline-hidden justify-center gap-1.5 rounded-lg px-4 py-2 text-zinc-400 transition-all bg-zinc-800 hover:bg-zinc-900 focus:bg-zinc-900"
               target="_blank"
             >
               <GithubFill size={14} />
