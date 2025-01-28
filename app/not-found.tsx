ce7728f115d9d2065f2d39fa01f4ace44837f765
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-28 18:50:17 +0530

    Not Found page added

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+import { type Metadata } from "next"
+import { Link } from "next-view-transitions"
+
+import generateMetadata from "@/utils/seo"
+
+export const metadata: Metadata = generateMetadata({
+  path: "/404/",
+  title: "Not Found | Roasted Studio | a design and engineering agency",
+  description:
+    "Cannot find the page you're looking for on Roasted Studio Website",
+})
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <main className="container my-40 min-h-[calc(100vh-450px)]">
+      <h1 className="mb-20 text-4xl font-semibold tracking-tight leading-normal text-zinc-100 md:text-6xl lg:text-7xl">
+        Not Found
+      </h1>
+      <div className="mb-40 font-medium">
+        The page you're looking for does not exists, go{" "}
+        <Link
+          href={"/"}
+          className="text-zinc-300 underline underline-offset-4 hover:no-underline"
+        >
+          Home
+        </Link>
+      </div>
+    </main>
+  )
+}
+
+export default NotFoundPage
