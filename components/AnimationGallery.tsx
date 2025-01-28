93e664188fcec601b2f82e2e66de61e5c3c22c4b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-28 15:50:01 +0530

    AnimationGallery: layout fixes

--- components/AnimationGallery.tsx
+++ components/AnimationGallery.tsx
@@ -16,35 +16,33 @@ const AnimationGallery: React.FC = () => {
 
   return (
     <>
-      <Container>
-        <div>
-          <div className="mt-5">
-            <Input
-              type="text"
-              id="search-input"
-              placeholder="Search Animation..."
-              value={search}
-              onChange={(e) => {
-                setSearch(e.target.value)
-              }}
-              icon={<Search size={15} />}
-            />
-          </div>
-          <div className="mt-5 grid gap-3 md:grid-cols-3 lg:grid-cols-5">
-            {allAnimations
-              .filter((animation) =>
-                animation.title
-                  .toLocaleLowerCase()
-                  .includes(search.toLocaleLowerCase())
-              )
-              .map((animation, index) => (
-                <AnimationCard
-                  key={index}
-                  animation={animation}
-                  setSelectedAnimation={setSelectedAnimation}
-                />
-              ))}
-          </div>
+      <Container className="mb-12">
+        <div className="mt-5">
+          <Input
+            type="text"
+            id="search-input"
+            placeholder="Search Animation..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value)
+            }}
+            icon={<Search size={15} />}
+          />
+        </div>
+        <div className="mt-5 grid gap-3 md:grid-cols-3 lg:grid-cols-5">
+          {allAnimations
+            .filter((animation) =>
+              animation.title
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            )
+            .map((animation, index) => (
+              <AnimationCard
+                key={index}
+                animation={animation}
+                setSelectedAnimation={setSelectedAnimation}
+              />
+            ))}
         </div>
       </Container>
       {selectedAnimation !== null && (
