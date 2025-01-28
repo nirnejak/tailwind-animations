4ffe932ab45085845b6bffb98e2e109aa4d848e7
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-28 15:41:26 +0530

    header: style updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,12 +15,14 @@ const Header: React.FC = () => {
       }}
     >
       <div className="mx-auto mt-5 max-w-[1080px] px-4">
-        <div className="flex flex-col items-center justify-between gap-3 md:flex-row">
-          <Link href={"/"} className="flex text-zinc-100">
-            <span className="font-light">Tailwind</span>
-            <span className="font-semibold">Animations</span>
+        <div className="flex flex-col items-center justify-between font-medium gap-3 md:flex-row">
+          <Link
+            href={"/"}
+            className="flex text-zinc-100 tracking-tighter font-medium"
+          >
+            Tailwind Animations
           </Link>
-          <p className="text-sm">
+          <p className="text-sm hidden md:flex gap-1">
             <span>Designed and built by </span>
             <Link
               href={"https://nirnejak.com/"}
